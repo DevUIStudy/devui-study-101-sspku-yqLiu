238db4f64aa48e22e55e194df31d00d5fca297d1
sspku-yqLiu <56879942+sspku-yqLiu@users.noreply.github.com>
2019-11-13 21:01:27 +0800

    修改表格到规定样式

--- yqLiu1900022816/src/app/components/table/table.component.ts
+++ yqLiu1900022816/src/app/components/table/table.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, HostBinding, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef,  ChangeDetectorRef } from '@angular/core';
 import { SourceType, originSource } from '../../../mock-data';
+import { TableExpandConfig } from 'data-table.model';
 
 
 @Component({
@@ -8,31 +9,64 @@ import { SourceType, originSource } from '../../../mock-data';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  @ViewChild('quickAddRowTip') quickAddRowTip: ElementRef;
+  @ViewChild('quickAddRowContent') quickAddRowContent: ElementRef;
+  @ViewChild('addSubRowContent') addSubRowContent: ElementRef;
   basicDataSource: Array<SourceType> = JSON.parse(JSON.stringify(originSource.slice(0, 10)));
   dataTableOptions = {
     columns: [
-        {
-            field: 'firstName',
-            header: 'First Name',
-            fieldType: 'text'
-        },
-        {
-            field: 'lastName',
-            header: 'Last Name',
-            fieldType: 'text'
-        },
-        {
-            field: 'gender',
-            header: 'gender',
-            fieldType: 'text'
-        },
-        {
-            field: 'dob',
-            header: 'Date of birth',
-            fieldType: 'date'
-        }
+      {
+        field: 'firstName',
+        header: '用户名称',
+        fieldType: 'text'
+      },
+      {
+        field: 'lastName',
+        header: '昵称',
+        fieldType: 'text'
+      },
+      {
+        field: 'tags',
+        header: '用户标签',
+        fieldType: 'text'
+      },
+      {
+        field: 'country',
+        header: '企业用户',
+        fieldType: 'text'
+      },
+      {
+        field: 'character',
+        header: '项目角色',
+        fieldType: 'text'
+      },
+      {
+        field: 'userid',
+        header: '账号体系',
+        fieldType: 'text'
+      },
+      {
+        field: 'disable',
+        header: '是否禁用',
+        fieldType: 'text'
+      },
+      {
+        field: 'dob',
+        header: '操作',
+        fieldType: 'date'
+      }
+      
     ]
-};
+  };
+  //最后一列的数据
+  genderSource = ['male', 'female'];
+  headerExpandConfig: TableExpandConfig;
+  defaultRowData = {
+    firstName: '',
+    lastName: '',
+    gender: 'Female',
+    dob: new Date(1991, 3, 1),
+  };
 
 
 
@@ -50,7 +84,7 @@ export class TableComponent implements OnInit {
     title: '项目管理',
     open: true,
     children: [
-      { title: '项目设置模板' ,active: true},
+      { title: '项目设置模板', active: true },
       { title: '设置项目创建者' },
       { title: '项目成员管理' },
     ]
@@ -59,14 +93,14 @@ export class TableComponent implements OnInit {
 
     children: [
       { title: '子内容1(禁用)', disabled: true },
-      { title: '子内容2(默认激活)',  },
+      { title: '子内容2(默认激活)', },
       { title: '子内容3' },
     ]
   }, {
     title: '私有依赖库'
   }];
 
-  /* 分页 */ 
+  /* 分页 */
 
   pager3 = {
     total: 648,
@@ -75,7 +109,7 @@ export class TableComponent implements OnInit {
   };
   tab1acticeID = 'tab1';
 
-  constructor(private ref: ChangeDetectorRef){
+  constructor(private ref: ChangeDetectorRef) {
 
   }
   ngOnInit() {
@@ -105,6 +139,37 @@ export class TableComponent implements OnInit {
   setIndex(number: number) {
     this.pager3.pageIndex = number;
     console.log(this.pager3.pageIndex);
+  }
+  ngAfterContentInit() {
+    this.headerExpandConfig = { expand: true, expandTemplateRef: this.quickAddRowTip };
+  }
+
+  newRow() {
+    this.headerExpandConfig.expandTemplateRef = this.quickAddRowContent;
+  }
+
+  quickRowAdded() {
+    const newData = { ...this.defaultRowData };
+    this.basicDataSource.unshift(newData);
+    this.headerExpandConfig.expandTemplateRef = this.quickAddRowTip;
+  }
+
+  quickRowCancel() {
+    this.headerExpandConfig.expandTemplateRef = this.quickAddRowTip;
+  }
+
+  addSubData(index, item) {
+    this.basicDataSource[index].expandConfig = { expand: true, expandTemplateRef: this.addSubRowContent };
+  }
+
+  subRowAdded(index, item) {
+    this.basicDataSource[index].expandConfig.expand = false;
+    const newData = { ...this.defaultRowData };
+    this.basicDataSource.splice(index + 1, 0, newData);
+  }
+
+  subRowCancel(index) {
+    this.basicDataSource[index].expandConfig.expand = false;
   }
 
 }
